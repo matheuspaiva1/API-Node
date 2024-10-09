9fb17f621fbce0fbdc89de0de89647841ced3116
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-10-09 15:00:35 -0300

    criando server com fastify

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 
-const server = createServer((req, res) => {
+// const server = createServer((req, res) => {
+//   res.write('hello world')  
 
-  res.write('hello world')  
+//   return res.end()
+// })
 
-  return res.end()
+// server.listen(3333)
+
+import {fastify} from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => {
+  return 'Hello World'
 })
 
-server.listen(3333)
+server.get('/users', () => {
+  return 'Hello users'
+})
+
+server.listen({
+  port: 3333,
+})
