c201c81649ff5c57e6011bfeea045b123e7eed98
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-10-10 14:19:01 -0300

    query parameters

--- database-memory.js
+++ database-memory.js
@@ -4,8 +4,9 @@ export class DatabaseMemory {
   #videos = new Map();
 
   // Set, Map
-    list(){
-      return Array.from(this.#videos.entries()).map((videoArray) => {
+    list(search){
+      return Array.from(this.#videos.entries())
+      .map((videoArray) => {
         const id = videoArray[0]
         const data = videoArray[1]
 
@@ -13,7 +14,13 @@ export class DatabaseMemory {
           id,
           ...data
         }
-      }) 
+      })
+      .filter(video => {
+        if(search){
+          return video.title.includes(search)
+        }
+        return true
+      })
     }
     
     create(video){
--- server.js
+++ server.js
@@ -28,8 +28,10 @@ server.post('/videos', (request, reply) => {
   return reply.status(201).send()
 })
 
-server.get('/videos', () => {
-  const videos = database.list()
+server.get('/videos', (request) => {
+  const search = request.query.search
+
+  const videos = database.list(search)
 
   return videos
 })
